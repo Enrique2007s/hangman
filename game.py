37b44ff8163ecdb5e82f822344db5b77f8e4b0c6
Enrique2007s <enriquesmith458@gmail.com>
2026-03-10 22:21:03 -0400

    added function to display word

--- game.py
+++ game.py
@@ -1,1 +1,13 @@
 from word_service import get_random_word
+
+MAX_ATTEMPTS = 6
+
+def display_word(word, guessed_letters):
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter + " "
+        else:
+            display += "_ "
+    
+    print("\n", display)
